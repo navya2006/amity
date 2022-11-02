658820ccd9aeaf63ae5e79da86677717045cb729
Shiven-2007 <shivenag@gmail.com>
2022-11-03 01:43:33 +0530

    nei nigga nei nei

--- pointer.js
+++ pointer.js
@@ -4,7 +4,7 @@ window.onload = function () {
 function getid(obj) {
   window.elem=(obj.id);
 }
-const animateTrailer = (e, interacting, boxt) => {
+const animateTrailer = (e, interacting, boxt, nextbox) => {
   const x = e.clientX - trailer.offsetWidth / 2,
         y = e.clientY - trailer.offsetHeight / 2;
   var act=document.getElementById(window.elem);
@@ -12,7 +12,8 @@ const animateTrailer = (e, interacting, boxt) => {
     transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`,
     height:`20px`,
     width:`20px`,
-    borderRadius:"100%"
+    borderRadius:"0",
+    backgroundColor:"transparent",
   }
   
   if (boxt){
@@ -25,10 +26,24 @@ const animateTrailer = (e, interacting, boxt) => {
       height:`${height}px`,
       width:`${width}px`,
       borderRadius:"0px",
+      backgroundColor:"#fff",
+    }
   }
-}
+  else if(nextbox){
+    var idk=act.getBoundingClientRect().left
+    var idk21=act.getBoundingClientRect().top
+    var height=act.getBoundingClientRect().height
+    var width=act.getBoundingClientRect().width
+    var keyframes ={
+      transform: `translate(${idk}px, ${idk21}px)`,
+      height:`${height}px`,
+      width:`${width}px`,
+      borderRadius:"0px",
+      backgroundColor:"transparent",
+    }
+  }
   trailer.animate(keyframes, { 
-    duration: 800, 
+    duration: 400, 
     fill: "forwards" 
   });
 }
@@ -49,9 +64,11 @@ window.onmousemove = e => {
         interacting = interactable !== null;
   const box= e.target.closest(".interactab") ,
         boxt=box!== null;
+  const nex= e.target.closest(".next-box"),
+        nextbox=nex!== null;
   const icon = document.getElementById("trailer-icon");
   
-  animateTrailer(e, interacting, boxt);
+  animateTrailer(e, interacting, boxt, nextbox);
   
   trailer.dataset.type = interacting ? interactable.dataset.type : "";
   
